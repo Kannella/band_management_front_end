72b73f3e44377bb0f4e405f92bafdd88ee10c269
guctn <uni.gustavoca@gmail.com>
2024-11-05 14:38:29 -0300

    Creating branch SignIn

--- app/src/App.js
+++ app/src/App.js
@@ -11,6 +11,9 @@ import BandManagerPage from './pages/manager/band_manager_page';
 import BookingManagerPage from './pages/manager/booking_manager_page';
 import CalendarManagerPage from './pages/manager/calendar_manager_page';
 
+import SignInPage from  './pages/sign_in_page';
+import SignUpPage from  './pages/sign_up_page';
+
 function App() {
   return (
     <Router>
@@ -18,6 +21,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<HomeManagerPage />} />
+          <Route path="/registration" element={<SignUpPage />} />
+          <Route path="/login" element={<SignInPage />} />
           <Route path="/bands" element={<BandManagerPage />} />
           <Route path="/bookings" element={<BookingManagerPage />} />
           <Route path="/calendar" element={<CalendarManagerPage />} />
--- app/src/pages/sign_in_page.jsx
+++ app/src/pages/sign_in_page.jsx
@@ -2,7 +2,7 @@ import React  from "react";
 
 
 function SignInPage(){
-    return <h1>Login</h1>
+    return <h1>Login Hello</h1>
 }
 
 export  default SignInPage;
