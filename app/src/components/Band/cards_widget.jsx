4daf9968adf8137840c1eb308e028b92ba8919d0
Eduarda Portel <eduarda.portel4@gmail.com>
2024-11-06 11:43:32 -0300

    add band-page scaffold

---
+++ app/src/components/Band/cards_widget.jsx
@@ -0,0 +1,9 @@
+// import React, { Component } from 'react';
+
+// class ButtonNirvana extends Component {
+//   render() {
+//     // ...
+//   }
+// }
+
+// export default Button; 
--- app/src/pages/manager/band_manager_page.jsx
+++ app/src/pages/manager/band_manager_page.jsx
@@ -1,8 +1,24 @@
-import React  from "react";
-
+import React from "react";
+import '../../assets/styles/styles.css';
 
 function BandManagerPage(){
-    return <h1>BandManager</h1>
+    return (
+        <div>
+            <h1>My bands</h1>
+
+            <div className="content">
+                <p> Ready, to create a band, click on the button bellow to start </p>
+            </div>
+            <button className="createButton">+ Create new band</button>
+
+            <div className="content">
+                <h2>Bands</h2>
+                <p>Manage all your bands, to chose onde click on the arrow inside the card</p>
+            </div>
+
+
+        </div>
+    ) 
 }
 
 export  default BandManagerPage;
