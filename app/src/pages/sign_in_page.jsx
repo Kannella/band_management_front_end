fd688b064a31acc814dcddea1e2ba00ac7a44321
guctn <uni.gustavoca@gmail.com>
2024-11-07 11:37:53 -0300

    Updating styles of the elements, it's just lacking the image

--- app/src/pages/sign_in_page.jsx
+++ app/src/pages/sign_in_page.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './sign_in_page.css';
-import logInSignUpImg from './assets/images/logInSignUpImg.png';
+import {logInSignUpImg} from './assets/images/logInSignUpImg.png'
 
 function SignInPage(){
     const [email, setEmail] = useState('');
@@ -10,13 +10,13 @@ function SignInPage(){
 
     return (
         <div className='screen container-fluid '>
-            <div className='formPart col-5 offset-md-2 align-self-center'>
+            <div className='formPart col-3 offset-md-2 align-self-center'>
                 <h1>Welcome Back</h1>
                 <p>Please enter your account details</p>
                 <form action="">
                     <p>Email</p>
                     <input
-                        className=''
+                        className='input'
                         type="email"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
@@ -24,27 +24,29 @@ function SignInPage(){
                     />    
                     <p>Password</p>
                     <input
+                        className='input'
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         required
                     />   
-                    <br/>
-                    <p>
-                        <Link to="/">forgot your password?</Link>
-                    </p>
-                    <br/>
+                    <Link className="noPassword" to="/">forgot your password?</Link>
+                    <br />
                     <button className='buttonLogIn' type='submit'>
                         Login
                     </button>
                 </form>
-                <p>Don't have an account? </p>
-                <button className='noAccountButton'>
-                    Sign Up
-                </button>
+                <br />
+                <div className='noAccountPart'>
+                    <p>Don't have an account? </p>
+                    <button className='noAccountButton'>
+                        Sign Up
+                    </button>
+                </div>
             </div>
+            <div className='division col-2'></div>
+
             <div className='photoPart col-5'>
-                <img src={logInSignUpImg} alt="" />
             </div>        
         </div>
     )
