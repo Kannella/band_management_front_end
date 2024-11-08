9bcd24db3acace96e9e54ddfe1a9f77f84c70c9a
Eduarda Portel <eduarda.portel4@gmail.com>
2024-11-08 14:49:39 -0300

    add bootstrap

--- app/src/pages/manager/band_manager_page.jsx
+++ app/src/pages/manager/band_manager_page.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../assets/styles/styles.css';
 
 function BandManagerPage(){
     return (
-        <div>
-            <h1>My bands</h1>
+        <div className="container mt-4">
+            <h1 className="text-center">My bands</h1>
 
-            <div className="content">
+            <div className="content text-center mb-4">
                 <p> Ready, to create a band, click on the button bellow to start </p>
             </div>
             <button className="createButton">+ Create new band</button>
 
-            <div className="content">
+            <div className="content mb-4">
                 <h2>Bands</h2>
                 <p>Manage all your bands, to chose onde click on the arrow inside the card</p>
             </div>
