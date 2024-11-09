125d1d02415e56c2d49520540075675daa2c0114
Ana Beatriz Schlink <abnschlink@gmail.com>
2024-11-08 21:29:41 -0300

    feat: add responsive navbar for mobile and tablet

--- app/src/components/nav_bar.jsx
+++ app/src/components/nav_bar.jsx
@@ -6,45 +6,47 @@ import { faCalendarDays, faTicket, faSquarePlus, faCircleUser } from '@fortaweso
 import { ReactComponent as Logo } from '../assets/images/logo_vector.svg';
 import { ReactComponent as HomeIcon } from '../assets/images/icon_home.svg';
 
-function NavBar() {
+function NavBar({ logoSize, iconSize, iconSizeHome }) { 
   return (
-    <div className="nav-container d-flex flex-column align-items-center" style={{ width: '6vw', height: '100vh' }}>
+    <div className="nav-container d-flex flex-column align-items-center" style={{
+      position: 'sticky',
+      top: 0,             
+      width: '6vw',        
+      height: '100vh',     
+      zIndex: 1000
+    }}>
       <div className="row w-100">
-        
-        {/* Logo no topo e centralizado horizontalmente */}
         <div className="col-12 d-flex flex-column justify-content-center align-items-center py-4 mb-5 mt-2">
-          <Logo width="40" height="40" />
+          <Logo width={logoSize} height={logoSize} />
         </div>
 
-        {/* Ícones no meio da navegação */}
         <div className="col-12 d-flex flex-column align-items-center justify-content-center flex-grow-1 py-5 mb-5 mt-2">
           <div className="icon-row py-3">
             <a href="#" className="icon-button icon-img">
-            <HomeIcon width="30" height="30" />
-            </a>
-          </div>
-          <div className="icon-row py-3">
-          <a href="#" className="icon-button">
-          <FontAwesomeIcon icon={faSquarePlus} size="2x" />
-          </a>
-          </div>
-          <div className="icon-row py-3">
-            <a href="#" className="icon-button">
-            <FontAwesomeIcon icon={faTicket} size="2x" />
+              <HomeIcon width={iconSizeHome} height={iconSizeHome} /> 
             </a>
           </div>
           <div className="icon-row py-3">
             <a href="#" className="icon-button">
-            <FontAwesomeIcon icon={faCalendarDays} size="2x" />
+              <FontAwesomeIcon icon={faSquarePlus} style={{fontSize: iconSize}} /> 
+            </a>
+          </div>
+          <div className="icon-row py-3">
+            <a href="#" className="icon-button">
+              <FontAwesomeIcon icon={faTicket} style={{fontSize: iconSize}} />
+            </a>
+          </div>
+          <div className="icon-row py-3">
+            <a href="#" className="icon-button">
+              <FontAwesomeIcon icon={faCalendarDays} style={{fontSize: iconSize}} />
             </a>
           </div>
         </div>
 
-        {/* Ícone do usuário na parte inferior */}
         <div className="col-12 d-flex flex-column align-items-center mt-5">
           <div className="icon-row mt-5 py-3">
             <a href="#" className="icon-button">
-            <FontAwesomeIcon icon={faCircleUser} size="2x" />
+              <FontAwesomeIcon icon={faCircleUser} style={{fontSize: iconSize}} />
             </a>
           </div>
         </div>
--- app/src/pages/manager/booking_manager_page.jsx
+++ app/src/pages/manager/booking_manager_page.jsx
@@ -1,16 +1,18 @@
 import React  from "react";
 
-import Header from "../../components/header";
-import NavBar from "../../components/nav_bar";
 
 
 function BookingPage(){
     return (
-    <div>
-        <Header>Booking</Header>
-
+<div className="d-flex pt-3">
+      <div >
+        <div className="title-page">Booking</div >
+        <div className="sub-text mt-3 mb-1">List of all the Events</div>
+        <hr />
+      </div>
     </div>
-    )
+  );
+    
 }
 
 export  default BookingPage;
