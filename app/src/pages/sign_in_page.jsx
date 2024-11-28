1548ac4b2d2d4c5f23ba80ca652880c78e2aeef6
guctn <162991063+guctn@users.noreply.github.com>
2024-11-28 19:58:03 -0300

    final version sign_in_page.jsx

--- app/src/pages/sign_in_page.jsx
+++ app/src/pages/sign_in_page.jsx
@@ -1,8 +1,64 @@
-import React  from "react";
-
+import React, { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './sign_in_page.css';
+import imgLogInSignUp from '../assets/images/imgLogInSignUp.png';
 
 function SignInPage(){
-    return <h1>Login</h1>
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+    const buttonSignUp = () => {
+        navigate('/registration');  
+    };
+
+
+    return (
+        <div className='screen container-fluid '>
+            <div className='formPart col-3 offset-md-2 align-self-center'>
+                <h1>Welcome Back</h1>
+                <p>Please enter your account details</p>
+                <form action="">
+                    <p>Email</p>
+                    <input
+                        className='inputLogIn'
+                        type="email"
+                        placeholder='name@example.com'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />    
+                    <p>Password</p>
+                    <input
+                        className='inputLogIn'
+                        type="password"
+                        placeholder='****'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />   
+                    <Link className="noPassword" to="/">forgot your password?</Link>
+                    <br />
+                    <button className='submitButton' type='submit'>
+                        Login
+                    </button>
+                </form>
+                <br />
+                <div className='noAccountPart'>
+                    <p>Don't have an account? </p>
+                    <button className='noAccountButton' onClick={buttonSignUp}>
+                        Sign Up
+                    </button>
+                </div>
+            </div>
+            <div className='division col-2'></div>
+
+            <div className='photoPart col-5'>
+                <img className='photoBandManager' src={imgLogInSignUp} alt="" />
+            </div>        
+        </div>
+    )
 }
 
 export  default SignInPage;
