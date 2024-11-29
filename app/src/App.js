3b9105258cedc0a3146f989e3070cdaad7111773
guctn <uni.gustavoca@gmail.com>
2024-11-29 19:11:43 -0300

    Calendar is responsive now

--- app/src/App.js
+++ app/src/App.js
@@ -16,6 +16,8 @@ import BookingTableManagerPage from './pages/manager/booking_table_manager_page'
 import BookingManagerPage from './pages/manager/booking_manager_page';
 import CalendarManagerPage from './pages/manager/calendar_manager_page';
 import BandDetailsPage from "./components/Band/band_details_page";
+import SignInPage from  './pages/sign_in_page';
+import SignUpPage from  './pages/sign_up_page';
 
 
 function App() {
@@ -50,6 +52,8 @@ function App() {
           >
             <Routes>
               <Route path="/" element={<HomeManagerPage />} />
+              <Route path="/registration" element={<SignUpPage />} />
+              <Route path="/login" element={<SignInPage />} />  
               <Route path="/bands" element={<BandManagerPage />} />
               <Route path="/bookings" element={<BookingManagerPage />} />
               <Route path="/bookings-table" element={<BookingTableManagerPage />} />
